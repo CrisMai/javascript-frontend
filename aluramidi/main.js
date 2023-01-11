332d6d57a2d4c93777ba3c76fedbfed4f9673f7b
CrisMai <crismai.dev@gmail.com>
2023-01-11 16:20:05 -0300

    Repetição otimizada com For

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -5,8 +5,7 @@ function tocaSom(idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
 
@@ -18,7 +17,5 @@ while (contador < listaDeTeclas.length) {
         tocaSom(idAudio);
     }
 
-    contador = contador + 1;
-  
 }
 
