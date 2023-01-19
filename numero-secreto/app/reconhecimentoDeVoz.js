d5db9ee6289a9d99a11cd4b7581da47aea974552
unknown <crismai.dev@gmail.com>
2023-01-19 15:57:48 -0300

    Exibindo a mensagem no console

--- numero-secreto/app/reconhecimentoDeVoz.js
+++ numero-secreto/app/reconhecimentoDeVoz.js
@@ -4,3 +4,10 @@ const recognition = new SpeechRecognition();
 recognition.lang = "pt-Br";
 
 recognition.start();
+
+recognition.addEventListener("result", onSpeak);
+
+function onSpeak(evento) {
+    console.log(evento.results[0][0].transcript);
+
+}
