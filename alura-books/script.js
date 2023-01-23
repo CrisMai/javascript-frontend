8e945540faf0fafa4cdc85e4d058b1ed250cfe41
unknown <crismai.dev@gmail.com>
2023-01-23 16:23:29 -0300

    Evitando problemas

--- alura-books/script.js
+++ alura-books/script.js
@@ -1,11 +1,15 @@
 
 async function buscaEndereco(cep) {
+  
+    var mensagemErro = document.getElementById("erro");
+    mensagemErro.innerHTML = "";
+
   try {
     var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json`);
     var consultaCEPConvertida = await consultaCEP.json();
 
     if (consultaCEPConvertida.erro) {
-        throw Error("CEP não existente!");
+      throw Error("CEP não existente!");
     }
 
     var cidade = document.getElementById("cidade");
@@ -16,15 +20,13 @@ async function buscaEndereco(cep) {
     logradouro.value = consultaCEPConvertida.logradouro;
     estado.value = consultaCEPConvertida.uf;
 
-
     console.log(consultaCEPConvertida);
     return consultaCEPConvertida;
   } catch (erro) {
-
+    mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`
     console.log(erro);
   }
 }
 
 var cep = document.getElementById("cep");
 cep.addEventListener("focusout", () => buscaEndereco(cep.value));
-
