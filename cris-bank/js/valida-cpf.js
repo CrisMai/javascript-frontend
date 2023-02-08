f91a38eea50bb1bdcf409888aecfce586337c37a
unknown <crismai.dev@gmail.com>
2023-02-08 15:14:03 -0300

    Validar primeiro digito-cpf

--- cris-bank/js/valida-cpf.js
+++ cris-bank/js/valida-cpf.js
@@ -3,6 +3,7 @@ export default function ehUmCPF(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
 
     validaNumeroRepetidos(cpf);
+    validaPrimeiroDigito(cpf);
     console.log(validaNumeroRepetidos(cpf));
 }
 
@@ -21,4 +22,21 @@ function validaNumeroRepetidos(cpf) {
     ]
 
     return numeroRepetidos.includes(cpf);
+}
+
+function validaPrimeiroDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+
+    for (let tamanho = 0; tamanho < 9; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+    return soma != cpf[9];
 }
