8a7d6d86e4ea1fe944ff92525ba89e3fbb9858b4
unknown <crismai.dev@gmail.com>
2023-02-08 14:56:40 -0300

    Verificar CPFs com números repetidos

--- cris-bank/js/script.js
+++ cris-bank/js/script.js
@@ -1,3 +1,4 @@
+import ehUmCPF from "./valida-cpf.js";
 
 const camposDoFormulario = document.querySelectorAll("[required]");
 
@@ -6,5 +7,7 @@ camposDoFormulario.forEach((campo) => {
 })
 
 function verificaCampo(campo) {
-    
+    if (campo.name == "cpf" && campo.value.length >= 11) {
+        ehUmCPF(campo);
+    }
 }
