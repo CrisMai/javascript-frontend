e0dc58c47a0c226ddcbdec342caa165cc4163a92
unknown <crismai.dev@gmail.com>
2023-02-15 14:40:53 -0300

    Promises

--- node/index.js
+++ node/index.js
@@ -5,8 +5,16 @@ function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
 }
 
-
 function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
+    fs.promises
+        .readFile(caminhoDoArquivo, encoding)
+        .then((texto) => console.log(chalk.green(texto)))
+        .catch(trataErro)
+}
+
+
+/* function pegaArquivo(caminhoDoArquivo) {
     const encoding = 'utf-8';
     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
         if (erro) {
@@ -14,7 +22,7 @@ function pegaArquivo(caminhoDoArquivo) {
         }
         console.log(chalk.green(texto));
     })
-}
+} */
 
 pegaArquivo('./arquivos/texto.md');
 
