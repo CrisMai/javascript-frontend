fd68f357fe791917328b214c7dda6920c96f7b77
unknown <crismai.dev@gmail.com>
2023-02-15 15:02:06 -0300

    Async/await

--- node/index.js
+++ node/index.js
@@ -5,24 +5,29 @@ function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
 }
 
-function pegaArquivo(caminhoDoArquivo) {
+// async/await
+async function pegaArquivo(caminhoDoArquivo) {
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+        console.log(chalk.green(texto));
+    }
+    catch (erro) {
+        trataErro(erro)
+    }
+   
+}
+
+// promisses com then()
+/* function pegaArquivo(caminhoDoArquivo) {
     const encoding = 'utf-8';
     fs.promises
         .readFile(caminhoDoArquivo, encoding)
         .then((texto) => console.log(chalk.green(texto)))
         .catch(trataErro)
 }
-
-
-/* function pegaArquivo(caminhoDoArquivo) {
-    const encoding = 'utf-8';
-    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-        if (erro) {
-            trataErro(erro)
-        }
-        console.log(chalk.green(texto));
-    })
-} */
+ */
 
 pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos/');
 
