0bd49a96d2dd06538c518d1dbd3298bef4f9f549
unknown <crismai.dev@gmail.com>
2023-02-22 13:39:29 -0300

    Gerando uma lista de URLs

--- node/src/http-validacao.js
+++ node/src/http-validacao.js
@@ -1,4 +1,9 @@
 
+function extraiLinks(arrLinks) {
+    return arrLinks.map((objetoLink) => Object.values(objetoLink).join())
+}
+
+
 export default function listaValidada(listaDeLinks) {
-    return "entrou na função";
+    return extraiLinks(listaDeLinks);
 }
